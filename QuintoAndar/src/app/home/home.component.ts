ef025eb3a5cd70a401b8f0a39b9eb92598b173c9
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-06-27 22:20:59 -0300

    Campo de grid com Sass
    Começado a primeira parte de conteúdo da Home, fora o header.

--- QuintoAndar/src/app/home/home.component.ts
+++ QuintoAndar/src/app/home/home.component.ts
@@ -12,26 +12,31 @@ export class HomeComponent implements OnInit {
 
   selectToHireOrBuyPrices!: HTMLElement;
   labelToHireOrBuyPrices!: HTMLElement;
+  
+  iconSelectedToHireElement!: HTMLElement;
+  iconSelectedToBuyElement!: HTMLElement;
 
-  constructor(@Inject(DOCUMENT) private document: Document ) {
+  constructor(@Inject(DOCUMENT) private document: Document) {
 
   }
 
   ngOnInit(): void {
     this.selectToHireOrBuyPrices = (this.document.getElementById("toHireOrBuyPrices") as HTMLElement);
     this.labelToHireOrBuyPrices = (this.document.getElementById("label-to-hire-or-buy") as HTMLElement);
-    
+
+    this.iconSelectedToHireElement = (this.document.getElementById("to-hire") as HTMLElement);
+    this.iconSelectedToBuyElement = (this.document.getElementById("to-buy") as HTMLElement);
   }
 
-  btnTohire() {
+  btnToHire() {
     let maxSearchValue0 = 1000;
     let maxSearchValue1 = 2000;
     let maxSearchValue2 = 3000;
     let maxSearchValue3 = 4000;
     let maxSearchValue4 = 5000;
 
-    this.selectToHireOrBuyPrices.innerHTML = 
-    `<option value="" selected disabled hidden>Escolha o valor</option>
+    this.selectToHireOrBuyPrices.innerHTML =
+      `<option value="" selected disabled hidden>Escolha o valor</option>
     <option>Aluguel ${maxSearchValue0}</option>
     <option>Aluguel ${maxSearchValue1}</option>
     <option>Aluguel ${maxSearchValue2}</option>
@@ -39,6 +44,11 @@ export class HomeComponent implements OnInit {
     <option>Aluguel ${maxSearchValue4}</option>`;
 
     this.labelToHireOrBuyPrices.innerText = "Aluguel até";
+
+    if (!this.iconSelectedToHireElement.classList.contains('selected-mode')) {
+      this.iconSelectedToBuyElement.classList.remove('selected-mode');
+      this.iconSelectedToHireElement.classList.add('selected-mode');
+    }
   }
 
   btnToBuy() {
@@ -48,8 +58,8 @@ export class HomeComponent implements OnInit {
     let maxSearchValue3 = 4000;
     let maxSearchValue4 = 5000;
 
-    this.selectToHireOrBuyPrices.innerHTML = 
-    `<option value="" selected disabled hidden>Escolha o valor</option>
+    this.selectToHireOrBuyPrices.innerHTML =
+      `<option value="" selected disabled hidden>Escolha o valor</option>
     <option>Imóvel ${maxSearchValue0}</option>
     <option>Imóvel ${maxSearchValue1}</option>
     <option>Imóvel ${maxSearchValue2}</option>
@@ -57,5 +67,10 @@ export class HomeComponent implements OnInit {
     <option>Imóvel ${maxSearchValue4}</option>`;
 
     this.labelToHireOrBuyPrices.innerText = "Imóvel até";
+
+    if (!this.iconSelectedToBuyElement.classList.contains('selected-mode')) {
+      this.iconSelectedToHireElement.classList.remove('selected-mode');
+      this.iconSelectedToBuyElement.classList.add('selected-mode');
+    }
   }
 }
