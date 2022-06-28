1919fde7669095b7cb98a7d468d2b18ab749ff37
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-06-28 14:24:09 -0300

    Componentizado os domains e ajustado os grid
    .

--- QuintoAndar/src/app/app.module.ts
+++ QuintoAndar/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { Content1Component } from './home/content1/content1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NavBarComponent,
+    Content1Component
   ],
   imports: [
     BrowserModule,
