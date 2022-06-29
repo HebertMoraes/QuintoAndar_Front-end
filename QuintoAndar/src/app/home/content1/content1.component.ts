478b792b49fd54ed6743876bcaa0f39c72051190
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-06-29 14:38:21 -0300

    transição carrosel
    iniciado o .ts de transição do carrosel

--- QuintoAndar/src/app/home/content1/content1.component.ts
+++ QuintoAndar/src/app/home/content1/content1.component.ts
@@ -7,17 +7,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Content1Component implements OnInit {
 
+  slides!: HTMLElement;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.slides = (document.getElementById("slides") as HTMLElement);
   }
 
   public goNavLeft (){
-
+    this.slides.style.transition = "transform 1s";
+    this.slides.style.transform = "translateX(0)";
   }
 
   public goNavRight(){
     
+    this.slides.style.transition = "transform 1s";
+    this.slides.style.transform = "translateX(-100%)";
   }
-
 }
