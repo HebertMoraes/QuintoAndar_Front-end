d38263c1d7784b79767e56633a8bf72db166abcc
Hebert Moraes <70861445+HebertMoraes@users.noreply.github.com>
2022-07-05 10:42:52 -0300

    Aplicado ViewChild
    Usado ViewChild onde da, no lugar de pesquisar no html pelo ID do elemento.

--- QuintoAndar/src/app/home/content5/content5.component.ts
+++ QuintoAndar/src/app/home/content5/content5.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { filter, map } from 'rxjs';
 import { Residence } from 'src/app/entities/residence';
 import { ResidenceService } from 'src/app/services/residence.service';
@@ -14,69 +14,61 @@ export class Content5Component implements OnInit {
   @Input() listOfResidence!: Residence[];
   @Output() updateListResidence = new EventEmitter();
 
-  slides!: HTMLElement;
-
   posSlides: number = 0;
   distanceSlideMov: number = 25;
 
   selectStatesHeader!: HTMLSelectElement;
-  selectStatesContent5!: HTMLSelectElement;
 
-  btnFilterToHire!: HTMLElement;
-  btnFilterToBuy!: HTMLElement;
+  @ViewChild('btnToHireContent5') btnFilterToHire!: ElementRef;
+  @ViewChild('btnToBuyContent5') btnFilterToBuy!: ElementRef;
+  @ViewChild('selectStatesContent5') selectStatesContent5!: ElementRef;
+  @ViewChild('slidesContent5') slides!: ElementRef
 
   constructor() { }
 
   ngOnInit(): void {
 
-    this.slides = (document.getElementById("slides-content-5") as HTMLElement);
-
     this.selectStatesHeader = (document.getElementById("select-states-header") as HTMLSelectElement);
-    this.selectStatesContent5 = (document.getElementById("select-states-content-5") as HTMLSelectElement);
-
-    this.btnFilterToHire = (document.getElementById("btn-to-hire-content5") as HTMLElement);
-    this.btnFilterToBuy = (document.getElementById("btn-to-buy-content5") as HTMLElement);
   }
 
   public btnToHire() {
 
-    if (!this.btnFilterToHire.classList.contains('selected')) {
-      this.btnFilterToBuy.classList.remove('selected');
-      this.btnFilterToHire.classList.add('selected');
+    if (!this.btnFilterToHire.nativeElement.classList.contains('selected')) {
+      this.btnFilterToBuy.nativeElement.classList.remove('selected');
+      this.btnFilterToHire.nativeElement.classList.add('selected');
     }
-    this.updateListResidence.emit(this.selectStatesContent5.value);
+    this.updateListResidence.emit(this.selectStatesContent5.nativeElement.value);
   }
 
   public btnToBuy() {
 
-    if (!this.btnFilterToBuy.classList.contains('selected')) {
-      this.btnFilterToHire.classList.remove('selected');
-      this.btnFilterToBuy.classList.add('selected');
+    if (!this.btnFilterToBuy.nativeElement.classList.contains('selected')) {
+      this.btnFilterToHire.nativeElement.classList.remove('selected');
+      this.btnFilterToBuy.nativeElement.classList.add('selected');
     }
-    this.updateListResidence.emit(this.selectStatesContent5.value);
+    this.updateListResidence.emit(this.selectStatesContent5.nativeElement.value);
   }
 
   public goNavLeft() {
 
-    this.slides.style.transition = "transform 1s";
     if (this.posSlides < 0) {
       this.posSlides += this.distanceSlideMov;
-      this.slides.style.transform = `translateX(${this.posSlides}%)`;
+      this.slides.nativeElement.style.transform = `translateX(${this.posSlides}%)`;
     }
   }
 
   public goNavRight() {
-    this.slides.style.transition = "transform 1s";
+
     if (this.posSlides > -140) {
       this.posSlides -= this.distanceSlideMov;
-      this.slides.style.transform = `translateX(${this.posSlides}%)`;
+      this.slides.nativeElement.style.transform = `translateX(${this.posSlides}%)`;
     }
   }
 
   public choiceState() {
 
-    this.selectStatesHeader.value = this.selectStatesContent5.value;
+    this.selectStatesHeader.value = this.selectStatesContent5.nativeElement.value;
 
-    this.updateListResidence.emit(this.selectStatesContent5.value);
+    this.updateListResidence.emit(this.selectStatesContent5.nativeElement.value);
   }
 }
--- QuintoAndar/src/app/home/header/header.component.ts
+++ QuintoAndar/src/app/home/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Residence } from 'src/app/entities/residence';
 
 @Component({
@@ -13,12 +13,11 @@ export class HeaderComponent implements OnInit {
 
   selectStatesHeader!: HTMLSelectElement;
   selectStatesContent5!: HTMLSelectElement;
-
-  selectToHireOrBuyPrices!: HTMLElement;
-  labelToHireOrBuyPrices!: HTMLElement;
-
-  iconSelectedToHireElement!: HTMLElement;
-  iconSelectedToBuyElement!: HTMLElement;
+  
+  @ViewChild('toHireHeader') iconSelectedToHire!: ElementRef;
+  @ViewChild('toBuyHeader') iconSelectedToBuy!: ElementRef;
+  @ViewChild('toHireOrBuyPrices') selectToHireOrBuyPrices!: ElementRef
+  @ViewChild('labelToHireOrBuy') labelToHireOrBuyPrices!: ElementRef
 
   maxValuesToSearch = [1000, 2000, 3000, 4000, 5000];
   
@@ -27,17 +26,11 @@ export class HeaderComponent implements OnInit {
   ngOnInit(): void {
     this.selectStatesHeader = (document.getElementById("select-states-header") as HTMLSelectElement);
     this.selectStatesContent5 = (document.getElementById("select-states-content-5") as HTMLSelectElement);
-
-    this.selectToHireOrBuyPrices = (document.getElementById("toHireOrBuyPrices") as HTMLElement);
-    this.labelToHireOrBuyPrices = (document.getElementById("label-to-hire-or-buy") as HTMLElement);
-
-    this.iconSelectedToHireElement = (document.getElementById("to-hire") as HTMLElement);
-    this.iconSelectedToBuyElement = (document.getElementById("to-buy") as HTMLElement);
   }
 
   btnToHire() {
 
-    this.selectToHireOrBuyPrices.innerHTML =
+    this.selectToHireOrBuyPrices.nativeElement.innerHTML =
       `<option value="" selected disabled hidden>Escolha o valor</option>
     <option>Aluguel ${this.maxValuesToSearch[0]}</option>
     <option>Aluguel ${this.maxValuesToSearch[1]}</option>
@@ -45,17 +38,17 @@ export class HeaderComponent implements OnInit {
     <option>Aluguel ${this.maxValuesToSearch[3]}</option>
     <option>Aluguel ${this.maxValuesToSearch[4]}</option>`;
 
-    this.labelToHireOrBuyPrices.innerText = "Aluguel até";
+    this.labelToHireOrBuyPrices.nativeElement.innerText = "Aluguel até";
 
-    if (!this.iconSelectedToHireElement.classList.contains('selected-mode')) {
-      this.iconSelectedToBuyElement.classList.remove('selected-mode');
-      this.iconSelectedToHireElement.classList.add('selected-mode');
+    if (!this.iconSelectedToHire.nativeElement.classList.contains('selected-mode')) {
+      this.iconSelectedToBuy.nativeElement.classList.remove('selected-mode');
+      this.iconSelectedToHire.nativeElement.classList.add('selected-mode');
     }
   }
 
   btnToBuy() {
 
-    this.selectToHireOrBuyPrices.innerHTML =
+    this.selectToHireOrBuyPrices.nativeElement.innerHTML =
       `<option value="" selected disabled hidden>Escolha o valor</option>
     <option>Imóvel ${this.maxValuesToSearch[0]}</option>
     <option>Imóvel ${this.maxValuesToSearch[1]}</option>
@@ -63,11 +56,11 @@ export class HeaderComponent implements OnInit {
     <option>Imóvel ${this.maxValuesToSearch[3]}</option>
     <option>Imóvel ${this.maxValuesToSearch[4]}</option>`;
 
-    this.labelToHireOrBuyPrices.innerText = "Imóvel até";
+    this.labelToHireOrBuyPrices.nativeElement.innerText = "Imóvel até";
 
-    if (!this.iconSelectedToBuyElement.classList.contains('selected-mode')) {
-      this.iconSelectedToHireElement.classList.remove('selected-mode');
-      this.iconSelectedToBuyElement.classList.add('selected-mode');
+    if (!this.iconSelectedToBuy.nativeElement.classList.contains('selected-mode')) {
+      this.iconSelectedToHire.nativeElement.classList.remove('selected-mode');
+      this.iconSelectedToBuy.nativeElement.classList.add('selected-mode');
     }
   }
 
